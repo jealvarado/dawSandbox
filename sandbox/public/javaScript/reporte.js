d9f01536bf6b692a6e3e07aee0f212f89341e11b
Ruddy <moncay.ruddy@gmail.com>
2017-02-21 14:16:11 -0500

    modificando algunas cosas de modulo de reporte

--- sandbox/public/javaScript/reporte.js
+++ sandbox/public/javaScript/reporte.js
@@ -25,28 +25,16 @@ $(document).ready(function () {
             }
         },
         series: [{
-            name: 'Brands',
+            name: 'Raquel',
             colorByPoint: true,
             data: [{
-                name: 'Microsoft Internet Explorer',
+                name: 'Ejercicios Resueltos',
                 y: 56.33
             }, {
-                name: 'Chrome',
-                y: 24.03,
+                name: 'Ejercicios No Resueltos',
+                y: 43.67,
                 sliced: true,
                 selected: true
-            }, {
-                name: 'Firefox',
-                y: 10.38
-            }, {
-                name: 'Safari',
-                y: 4.77
-            }, {
-                name: 'Opera',
-                y: 0.91
-            }, {
-                name: 'Proprietary or Undetectable',
-                y: 0.2
             }]
         }]
     });
