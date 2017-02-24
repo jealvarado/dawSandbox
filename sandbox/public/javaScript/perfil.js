f227522d787ee20db0fe56ef7d8da093a03dc867
Ruddy <moncay.ruddy@gmail.com>
2017-02-24 12:35:49 -0500

    js perfil

--- sandbox/public/javaScript/perfil.js
+++ sandbox/public/javaScript/perfil.js
@@ -4,7 +4,14 @@ $document.ready(function({
        url ="json/perfil.json";
 
         $.getJSON(url, function(resp){
+        	$("#name").html(resp.nombre);
+        	$("#puntaje").html(resp.puntaje_obtenido);
+        	$("#ejercicios").html(resp.ejercicioscompletados);
 
+        	var insig = $("#insignias");
+        	resp.insignias.forEach(function(i){
+        		insig.add($("<li>",{"class":i.nombre}).html(i.nombre));
+        	});
         });
 
     })
