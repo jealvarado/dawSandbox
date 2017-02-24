b0c0ca951bb957b93959c4087e5f65cbd626dfca
Ruddy <moncay.ruddy@gmail.com>
2017-02-24 14:28:42 -0500

    creando un js del reporte estadistico del curso

---
+++ sandbox/public/javaScript/curso_reporte.js
@@ -0,0 +1,44 @@
+        var url = "json/curso.json" // esta url debe ser generada con la fecha 
+        $.getJSON(url, function(resp){
+            r=resp;
+            //tutorial del grafico 
+            var datos = []
+            resp.data.forEach(function(i){
+                datos.push(
+                {
+                    
+                    y:Number(i.ejercicios)
+                });
+            });
+            Highcharts.chart('container', {
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false,
+                    type: 'pie'
+                },
+                title: {
+                    text: 'Browser market shares January, 2015 to May, 2015'
+                },
+                tooltip: {
+                    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: false
+                        },
+                        showInLegend: true
+                    }
+                },
+                series: [{
+                    name: 'Brands',
+                    colorByPoint: true,
+                    data:datos
+                    }]
+                });
+            });
+            //
+        });
