1ff05739115289c9ab0803e77234c87cd5e7d9b9
Erick Pérez <erialper@espol.edu.ec>
2017-03-15 17:37:59 -0500

    perfil total

--- sandbox/public/javaScript/perfil.js
+++ sandbox/public/javaScript/perfil.js
@@ -13,7 +13,7 @@ function LoadData() {
                 url:'/api/usuario/'+perfil.perfil[0].idestudiante,
                 type: 'GET',
                 success:function(user){
-                    //console.log(user);
+                    console.log(user);
                     $("#nickUser").append($("<strong>").text(user.usuario.nombre + ' ' + user.usuario.apellido));
                     $("#dataUser").append($("<p>").text("Paralelo: " + user.usuario.paralelo));
                     $("#dataUser").append($("<p>").text("Carrera: " + user.usuario.carrera));
@@ -22,7 +22,7 @@ function LoadData() {
                     $("#dataUser").append($("<p>").text("Puntaje Ej. Dificil: " + perfil.perfil[0].ejDificil*15));
                     var sum = perfil.perfil[0].ejFacil*5 + perfil.perfil[0].ejIntermedio*10 + perfil.perfil[0].ejDificil*15;
                     $("#datatotal").append($("<strong>").text("Total: " + sum));
-                    insigniaSemana("58b7458b56da5a0f90bb6067");
+                    insigniaSemana(user.usuario._id);
                 }
             })
         }
@@ -35,9 +35,10 @@ function insigniaSemana(clave){
         type: 'GET',
         success:function(resuelto){
             console.log(resuelto);
-            var cont = 0;
+            var contS = 0;
             var f = new Date()
             var act = saberSemana(f.getDate(),f.getMonth(),f.getFullYear())
+            var contE = resuelto.resuelto.length; 
             resuelto.resuelto.forEach(function(i){
                 var fecha = i.fecha.split("T")
                 var fe = fecha[0].split("-")
@@ -46,11 +47,27 @@ function insigniaSemana(clave){
                 var dia = fe[2];
                 var pas = new Date(ano,mes,dia)
                 var con = saberSemana(pas.getDate(),pas.getMonth(),pas.getFullYear());
-                if(act==cont){
-                    cont++;
+                if(act==con){
+                    contS++;
                 }
-                console.log(cont)
             })
+            console.log(contE)
+            if(contE<10&&contE<20){
+                $("#dataUser").append($("<strong>").text("Nivel: Novato "));
+            }else if (contE>=20&&contE<30){
+                $("#dataUser").append($("<strong>").text("Nivel: Pro "));
+            }else if(contE>30){
+                $("#dataUser").append($("<strong>").text("Nivel: Experto"));
+            }
+            $("#dataUser").append("<br>");
+            console.log(contS)
+            if(contS<5&&contS<10){
+                $("#dataUser").append($("<strong>").text("Insignia de la Semana: Indestructible "));
+            }else if (contS>=10&&contS<15){
+                $("#dataUser").append($("<strong>").text("Insignia de la Semana: Duro de Matar "));
+            }else if(contS>15){
+                $("#dataUser").append($("<strong>").text("Insignia de la Semana: Rapido y Furioso"));
+            }
         }
     })
 }
